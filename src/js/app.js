680d54b8a869dc787bf85e0cb7038c0f88d665a2
Karoll Arengas <kyarengasm@ufpso.edu.co>
2025-12-01 08:28:03 -0500

    Agregar código base de la SPA (index y app.js)

---
+++ src/js/app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            vistaActual: "home"
+        }
+    },
+    methods: {
+        cambiarVista(vista) {
+            this.vistaActual = vista;
+        }
+    }
+});
+
+// Componentes base (más adelante los llenamos con contenido)
+app.component("home", {
+    template: '<div><h2 class="text-center">Bienvenida a Fogón en Línea</h2></div>'
+});
+
+app.component("recetas", {
+    template: '<div><h2 class="text-center">Recetas</h2></div>'
+});
+
+app.component("contacto", {
+    template: '<div><h2 class="text-center">Contacto</h2></div>'
+});
+
+app.component("login", {
+    template: '<div><h2 class="text-center">Iniciar Sesión</h2></div>'
+});
+
+app.mount("#app");
